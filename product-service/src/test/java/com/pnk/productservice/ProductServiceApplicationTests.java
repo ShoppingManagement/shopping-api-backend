523c0b435afc496908a211383231f18b6a0dbdcd
Phong Vo <vhphong@gmail.com>
2024-03-18 15:00:32 -0500

    Add existing project files to Git

---
+++ product-service/src/test/java/com/pnk/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pnk.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
